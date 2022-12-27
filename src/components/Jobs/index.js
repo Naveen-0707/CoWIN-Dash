05867924cdfafa4bcb7bed07ae159b2b1222d754
naveen <pillalamarinaveenkumar@gmail.com>
2022-12-27 16:50:24 +0000

    jobby

---
+++ src/components/Jobs/index.js
@@ -0,0 +1,16 @@
+import AllJobsSection from '../AllJobSection'
+
+import Header from '../Header'
+
+import './index.css'
+
+const Jobs = () => (
+  <>
+    <Header />
+    <div className="product-sections">
+      <AllJobsSection />
+    </div>
+  </>
+)
+
+export default Jobs
